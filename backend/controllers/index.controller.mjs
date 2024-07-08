a2e5855456a085233b61502b4896bd1d7e948e21
SukumDecha <ninemaster12gt@gmail.com>
2024-07-08 16:50:48 +0700

    implement uploadLogic:

--- backend/controllers/index.controller.mjs
+++ backend/controllers/index.controller.mjs
@@ -6,6 +6,7 @@ import campaignRouter from "./campaign.controller.mjs";
 import blogRouter from "./blog.controller.mjs";
 import productRouter from "./product.controller.mjs";
 import commentRouter from "./comment.controller.mjs";
+import qrCodeRouter from "./qrcode.controller.mjs";
 import { handleGuard } from "./middlewares/jwt.middleware.mjs";
 
 const router = Router();
@@ -15,7 +16,7 @@ router.use(userRouter);
 router.use(campaignRouter);
 router.use(blogRouter);
 router.use(productRouter);
-
+router.use(qrCodeRouter);
 router.use(handleGuard, commentRouter);
 
 export default router;
